1ad96eb1dec867e56a065692abd4324cf18c9eb0
Mehrdod Ilhompur <71515606+IM1797@users.noreply.github.com>
2022-11-11 22:27:55 -0500

    Update MAV.java

--- MAV.java
+++ MAV.java
@@ -1,13 +1,3 @@
-// -----------------------------------------------------
-// Assignment 2
-// Part 1 
-// Written by: Mehrdod Ilhompur 40078633 and David R Cronin 28840024
-// -----------------------------------------------------
-package drone;
-/**
- * 
- * @author Mehrdod
- */
 import Unmanned.UAV;
 
 public class MAV extends UAV{
