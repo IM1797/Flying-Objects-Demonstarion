10be71093a6583d98734a9d28738328a31ffd7f4
Mehrdod Ilhompur <71515606+IM1797@users.noreply.github.com>
2022-11-11 22:27:15 -0500

    Update FlyingObjects.java

--- FlyingObjects.java
+++ FlyingObjects.java
@@ -1,14 +1,3 @@
-// -----------------------------------------------------
-// Assignment 2
-// Part 1
-// Written by: Mehrdod Ilhompur 40078633 and David R Cronin 28840024
-// -----------------------------------------------------
-package airp;
-/**
- * 
- * @author Mehrdod
- *
- */
 //Superclass for all classes
 	public class FlyingObjects {
  		//The default constructor
